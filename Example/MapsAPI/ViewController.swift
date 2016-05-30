f381740de4ee969bf0aace4f9d9a8fab1c0d0a67
Ajaxy <alx.zinchuk@gmail.com>
2016-05-31 00:06:52 +0300

    Initial commit

---
+++ Example/MapsAPI/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MapsAPI
+//
+//  Created by Ajaxy on 05/31/2016.
+//  Copyright (c) 2016 Ajaxy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
