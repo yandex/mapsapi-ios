7afd33af97c486a72a47796123484a833210184b
Ajaxy <alx.zinchuk@gmail.com>
2016-07-31 10:47:03 +0300

    fix init script

--- defaults/myMap.js
+++ defaults/myMap.js
@@ -1,9 +1,9 @@
-ymaps.ready(['native']).then(function (ymaps, native) {
+ymaps.ready(['native']).then(function (ymaps) {
 
     var map = new ymaps.Map('map', {
             center: [55.7452, 37.5867],
             zoom: 12,
-            controls: native.controls
+            controls: ymaps.native.controls
         }),
         
         geoObject = new ymaps.Placemark(map.getCenter(), {
